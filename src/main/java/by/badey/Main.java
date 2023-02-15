fba378e7a8aba95aa0c66e7b6f275a462538c669
stasbadey <stas5511665@gmail.com>
2023-02-15 19:28:16 +0300

    task1

--- src/main/java/by/badey/Main.java
+++ src/main/java/by/badey/Main.java
@@ -35,114 +35,64 @@ public class Main {
 
     private static void task1() throws IOException {
         List<Animal> animals = Util.getAnimals();
-        animals.stream()
-                .filter(animal -> animal.getAge() > 10 && animal.getAge() < 20)
-                .sorted()
-                .skip(21)
-                .limit(7)
-                .forEach(System.out::println);
+        //        animals.stream() Продолжить ...
     }
 
     private static void task2() throws IOException {
         List<Animal> animals = Util.getAnimals();
-        animals.stream()
-                .filter(animal -> animal.getOrigin().equals("Japanese"))
-                .map(animal -> animal.getGender().equals("Female")
-                        ? animal.getBread().toUpperCase()
-                        : animal.getBread())
-                .forEach(System.out::println);
+        //        animals.stream() Продолжить ...
 
     }
 
     private static void task3() throws IOException {
         List<Animal> animals = Util.getAnimals();
-        animals.stream()
-                .filter(animal -> animal.getAge() > 30)
-                .map(Animal::getOrigin)
-                .filter(origin -> origin.startsWith("A"))
-                .distinct()
-                .forEach(System.out::println);
+        //        animals.stream() Продолжить ...
     }
 
     private static void task4() throws IOException {
         List<Animal> animals = Util.getAnimals();
-        long count = animals.stream()
-                .filter(animal -> animal.getGender().equals("Female"))
-                .count();
-        System.out.println(count);
+        //        animals.stream() Продолжить ...
 
     }
 
     private static void task5() throws IOException {
         List<Animal> animals = Util.getAnimals();
-        boolean hungarian = animals.stream()
-                .filter(animal -> animal.getAge() > 20 && animal.getAge() < 30)
-                .anyMatch(animal -> animal.getOrigin().equals("Hungarian"));
-        System.out.println(hungarian);
+        //        animals.stream() Продолжить ...
     }
 
     private static void task6() throws IOException {
         List<Animal> animals = Util.getAnimals();
-        boolean Gender = animals.stream()
-                .allMatch(animal -> animal.getGender().equals("Male")
-                        && animal.getGender().equals("Female"));
-        System.out.println(Gender);
+        //        animals.stream() Продолжить ...
     }
 
     private static void task7() throws IOException {
         List<Animal> animals = Util.getAnimals();
-        boolean oceania = animals.stream()
-                .anyMatch(animal -> !(animal.getOrigin().equals("Oceania")));
-        System.out.println(oceania);
+        //        animals.stream() Продолжить ...
     }
 
     private static void task8() throws IOException {
         List<Animal> animals = Util.getAnimals();
-        Animal animal = animals.stream()
-                .sorted(Comparator.comparing(Animal::getBread))
-                .limit(100)
-                .max(Comparator.comparing(Animal::getAge))
-                .orElse(null);
-        System.out.println(animal.getAge());
+        //        animals.stream() Продолжить ...
     }
 
     private static void task9() throws IOException {
         List<Animal> animals = Util.getAnimals();
-        char[] result = animals.stream()
-                .map(Animal::getBread)
-                .map(String::toCharArray)
-                .min(Comparator.comparing(chars -> chars.length))
-                .orElse(null);
-        System.out.println(result.length);
+        //        animals.stream() Продолжить ...
     }
 
     private static void task10() throws IOException {
         List<Animal> animals = Util.getAnimals();
-        int sum = animals.stream()
-                .mapToInt(Animal::getAge)
-                .sum();
-        System.out.println(sum);
+        //        animals.stream() Продолжить ...
     }
 
     private static void task11() throws IOException {
         List<Animal> animals = Util.getAnimals();
-        double average = animals.stream()
-                .filter(animal -> animal.getOrigin().equals("Indonesian"))
-                .mapToInt(Animal::getAge)
-                .average()
-                .getAsDouble();
-        System.out.println(average);
+        //        animals.stream() Продолжить ...
     }
 
     private static void task12() throws IOException {
         List<Person> people = Util.getPersons();
-        people.stream()
-                .filter(person -> person.getGender().equals("Male"))
-                .filter(person -> ChronoUnit.YEARS.between(person.getDateOfBirth(), LocalDate.now()) > 18
-                        && ChronoUnit.YEARS.between(person.getDateOfBirth(), LocalDate.now()) < 27)
-                .sorted(Comparator.comparing(Person::getRecruitmentGroup))
-                .limit(200)
-                .forEach(System.out::println);
+//        Продолжить...
     }
 
     private static void task13() throws IOException {
