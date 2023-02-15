2fa2d686d9742ec8ca233bfce75b521ceaf93d01
stasbadey <stas5511665@gmail.com>
2023-02-15 19:33:23 +0300

    task5

--- src/main/java/by/badey/Main.java
+++ src/main/java/by/badey/Main.java
@@ -53,7 +53,10 @@ public class Main {
 
     private static void task5() throws IOException {
         List<Animal> animals = Util.getAnimals();
-        //        animals.stream() Продолжить ...
+        boolean hungarian = animals.stream()
+                .filter(animal -> animal.getAge() > 20 && animal.getAge() < 30)
+                .anyMatch(animal -> animal.getOrigin().equals("Hungarian"));
+        System.out.println(hungarian);
     }
 
     private static void task6() throws IOException {
